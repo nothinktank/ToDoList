19cc59d65c3c1dec5128ac84cb6455d2f7d31736
shangwang <wangshangming10@gmail.com>
2025-04-01 00:28:59 -0400

    fixed bugs for local storage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
-import {Item, addNewItem, displayItems} from './item.js';
+import {Item, addNewItem, displayItems, displayItem} from './item.js';
 // import {Project, addNewProject} from './project.js';
 
 import createPopup from './newItemPopup.js';
@@ -23,7 +23,12 @@ const addProject = document.querySelector('.addProject');
   const secondItem = new Item('fix duplicate item appending issue', 'find where the duplication is', '3/5/2025','1', 'general' );
 
   
-  // general: [item, secondItem], general2: []
+  //load projectList from local storage
+
+
+
+
+
   let projectList = {};
   // generalProject.toDoItems.push('set up object name check during adding items');
   //display all projects
@@ -35,21 +40,65 @@ const addProject = document.querySelector('.addProject');
   let myProjects = document.querySelector('.myProjects');
 
   function displayProjects() {
+    myProjects.replaceChildren();
+    //get the number of item stored in local storage
+    let numberOfItemInStorage = localStorage.length;
+    
+
     let projectNameArray = Object.keys(projectList);
     let itemArray = Object.values(projectList);
+    let projectIdArray = [];
     console.log(itemArray); 
 
-    for (let i = 0; i < projectNameArray.length; i++) {
+//need to find all unique project names and populate all items with the same project name once
+for (let k = 0; k< numberOfItemInStorage; k++) {
+  let retrievedItemKey = localStorage.key(k);
+  let retrievedItemValue = localStorage.getItem(retrievedItemKey);
+  let parsedItem = JSON.parse(retrievedItemValue);
+  let projectId = parsedItem.project;
+  
+  if (!projectIdArray.includes(projectId)){
+    projectIdArray.push(projectId);
+    //projectIdArray is an array of unique project names
+  }
+}
+console.log(`projectIdArray is ${projectIdArray}`)//list the projects that needed to be displayed
+
+ //create the dom elements for the projects
+ //need to have title, remove button and a ul for each project, ul is for attaching items
+    for (let i = 0; i < projectIdArray.length; i++) {
+
+      
+      //   let retrievedItemKey = localStorage.key(i);
+      // //use the retrieved item key to get item value
+      //   let retrievedItemValue = localStorage.getItem(retrievedItemKey);
+      // //parse the item value
+      //   let parsedItem = JSON.parse(retrievedItemValue);
+
       let newProject = document.createElement('li');
       let removeProjectBtn = document.createElement('button');
       let projectTitle= document.createElement('div');
       let listOfTodoItems = document.createElement('ul');
 
-      projectTitle.textContent = projectNameArray[i];
-      //call the displayItem function here 
-      console.log(itemArray[i]);
-      displayItems(itemArray[i], listOfTodoItems, projectList);
-      console.log(projectList);
+      projectTitle.textContent = projectIdArray[i];
+      
+      // displayItems(parsedItem.project, listOfTodoItems, projectList);//display item separately
+
+      //for each project, find all items that has the same project name and display them
+      //save project id as a variable
+      
+
+        for (let p = 0; p < numberOfItemInStorage; p++){
+          let retrievedItemKey = localStorage.key(p);
+          let retrievedItemValue = localStorage.getItem(retrievedItemKey);
+          let parsedItem = JSON.parse(retrievedItemValue);
+
+          //find items under the project name === projectIdArray[i]
+          if (parsedItem.project === projectIdArray[i]){
+            displayItem(parsedItem, listOfTodoItems, retrievedItemKey);
+          }
+        }
+      
 
       myProjects.appendChild(newProject);
       newProject.appendChild(projectTitle);
@@ -57,27 +106,55 @@ const addProject = document.querySelector('.addProject');
       newProject.appendChild(listOfTodoItems);
 
       //remove projects logic 
+      //add attribute to removeProject button, the attribute would be the project name
       removeProjectBtn.textContent = 'Remove';
+      removeProjectBtn.setAttribute('nameOfProject', projectIdArray[i]);
       newProject.appendChild(removeProjectBtn);
 
-      let projectIndex = projectNameArray.indexOf(newProject);
+      let projectIndex = projectIdArray.indexOf(newProject);
       removeProjectBtn.addEventListener('click', (e) => {
         //need to operate on the projectList object and remove the key/value pair within the object
-        // let projectKey = projectNameArray[i];
-        console.log(projectTitle);
-        delete projectList[projectTitle.textContent];
-        console.log(projectList);
-        projectNameArray.splice(projectIndex, 1);
-        myProjects.removeChild(newProject);
-        console.log(projectList);
+
+        delete projectList[projectTitle.textContent]; //check if i still need to reference projectList
+        removeItemsUnderProject(projectIdArray[i]);
+        projectIdArray.splice(projectIndex, 1); //remove the project from the unique project name array
+        myProjects.removeChild(newProject); 
+        
+        //needs to remove all the items under the removed project local storage
       })
     }
+  }
+//a removeProject function that removes the items under the project from localStorage
+  function removeItemsUnderProject(projectTitle) {
+    for(let [key, value] of Object.entries(localStorage)){
+      let parsedValue = JSON.parse(value);
+      if (projectTitle === parsedValue.project){
+        localStorage.removeItem(key);
+      }
+    }
+
+
+
+    // let numberOfItemInStorage = localStorage.length;
+    // for (let i = 0; i < numberOfItemInStorage; i++) {
+    //   let retrievedItemKey = localStorage.key(i);
+    //   //use the retrieved item key to get item value
+    //     let retrievedItemValue = localStorage.getItem(retrievedItemKey);
+    //   //parse the item value
+    //     let parsedItem = JSON.parse(retrievedItemValue);
+    //     // console.log(parsedItem.project);
+
+    //     if (parsedItem.project === nameOfProject) {
+    //       console.log(i);
+    //       localStorage.removeItem(retrievedItemKey);
+    //     }
+    // }
   }
 
   displayProjects();
   addPopupFunctionToNewEditBtnsAndRemoveExistingItem();
 
-item.detail();
+
 
 document.querySelector('.submit-btn').addEventListener('click', ()=>{
   addNewItem(projectList, '#item-popup', '');
@@ -85,8 +162,9 @@ document.querySelector('.submit-btn').addEventListener('click', ()=>{
   //remove child elements from DOM for projects
   myProjects.replaceChildren();
   displayProjects();
-  console.log(projectList);
   addPopupFunctionToNewEditBtnsAndRemoveExistingItem();
+  console.log(projectList);
+  
 }  );
 
 //create similar code above next
@@ -95,8 +173,9 @@ document.querySelector('.submit-edits').addEventListener('click', ()=>{
   console.log(projectList);
   myProjects.replaceChildren();
   displayProjects();
-  console.log(projectList);
   addPopupFunctionToNewEditBtnsAndRemoveExistingItem();
+  console.log(projectList);
+  
 })
 
 //call the project popup modal on click
@@ -112,14 +191,13 @@ let editPopup = createPopup('#edit-popup');
 let listOfEditBtns = document.querySelectorAll('.open-edit-popup');
 // console.log(listOfEditBtns[0]);//get the actual value of button.open-edit-popup
 
-let titleProjectRef;
 
 //add event listener to each of the buttons in listOfEditBtns
 function addPopupFunctionToNewEditBtnsAndRemoveExistingItem(){
   let scopedListOfEditBtns = document.querySelectorAll('.open-edit-popup');
   for(let i = 0; i < scopedListOfEditBtns.length ; i++) {
   scopedListOfEditBtns[i].addEventListener('click', () => {
-  console.log(scopedListOfEditBtns[i].getAttribute('project-item'));
+  
   
     let title = document.querySelector('#edit-title');
     let description = document.querySelector('#edit-description');
@@ -129,23 +207,50 @@ function addPopupFunctionToNewEditBtnsAndRemoveExistingItem(){
 
   let btnIdentifier = scopedListOfEditBtns[i].getAttribute('project-item');
 
-  //use for in loop for the projectList object to access each item for edit
-    for (let project in projectList) {
-      for (let i = 0; i < projectList[project].length; i++ ) {
-        if (projectList[project][i].identifier === btnIdentifier) {
-          // fill the popup with existing data
-      title.value = projectList[project][i].title;
-      description.value = projectList[project][i].description;
-      dueDate.value = projectList[project][i].dueDate;
-      priority.value = projectList[project][i].priority;
-      projectName.value = projectList[project][i].project;
-      
-      //remove the selected item from ProjectList
-      projectList[project].splice(i, 1);
+  let numberOfStoredItems = localStorage.length;
+    //for loop to access the item with a matching attribute
+    if(numberOfStoredItems > 0){
+      for (let i = 0; i < numberOfStoredItems; i++) {
+        let retrievedItemKey = localStorage.key(i);
+        let retrievedItemValue = localStorage.getItem(retrievedItemKey);
+        let parsedItem = JSON.parse(retrievedItemValue);
+        // console.log(parsedItem.identifier);
+    // console.log(JSON.parse(localStorage.getItem(localStorage.key(0))));
+    if (parsedItem.identifier === btnIdentifier){
+      //restricted title and project for editing existing items
+      title.textContent = parsedItem.title;
+      projectName.textContent = parsedItem.project;
 
-        }
-      }
+      description.value = parsedItem.description;
+      dueDate.value = parsedItem.dueDate;
+      priority.value = parsedItem.priority;
+
+    
+      //testing my theory, return the parsedItem.identifier 
+
+    //remove existing item
+      // localStorage.removeItem(localStorage.key(i));
     }
+    }
+    }
+  
+  //use for in loop for the projectList object to access each item for edit
+    // for (let project in projectList) {
+    //   for (let i = 0; i < projectList[project].length; i++ ) {
+    //     if (projectList[project][i].identifier === btnIdentifier) {
+    //       // fill the popup with existing data
+    //   title.value = projectList[project][i].title;
+    //   description.value = projectList[project][i].description;
+    //   dueDate.value = projectList[project][i].dueDate;
+    //   priority.value = projectList[project][i].priority;
+    //   projectName.value = projectList[project][i].project;
+      
+    //   //remove the selected item from ProjectList
+    //   projectList[project].splice(i, 1);
+
+    //     }
+    //   }
+    // }
   editPopup();
 })
 }
@@ -153,8 +258,12 @@ function addPopupFunctionToNewEditBtnsAndRemoveExistingItem(){
 }
 
 //detects whether localStorage is both supported and available
-if(storageAvailable('localStorage')) {
+if(storageAvailable('sessionStorage')) {
   console.log('available');
 }else {
   console.log('storage not available')
 }
+
+
+
+// bug- remove an entire project is not removing the item in local storage
--- src/item.js
+++ src/item.js
@@ -18,53 +18,66 @@ export class Item {
   }
 
   export function addNewItem(projectList, typeOfPopup, edit) {
-    // let popupNode = document.querySelector('#item-popup');
+    //if edit is not blank in the input, need to remove the previous record, or overwrite the previous record
+
     let popupNode = document.querySelector(typeOfPopup);
     let title = document.querySelector( `#${edit}title`);
     let description = document.querySelector(`#${edit}description`);
     let dueDate = document.querySelector(`#${edit}duedate`);
     let priority = document.querySelector(`#${edit}priority`);
     let project = document.querySelector(`#${edit}project`);
-
-    //if title field is not blank then initialize a new item using item class
-    if (title.value){
-      let newItem = new Item(title.value, description.value, dueDate.value, priority.value, project.value)
-      console.log(newItem);
     
-      let projectNameArray = Object.keys(projectList);
-      if (projectNameArray.includes(project.value)){ //if the item project name already exists
-        for (let i = 0; i < projectNameArray.length; i++) { 
-          if (project.value === projectNameArray[i]){ //find the name of the existing project in projectList
-            let matchingProjectName = project.value;
-            projectList[matchingProjectName].push(newItem);//add newItem to the project value array
-             }
-          }
-        } else { //when includes() returns false
-          let newProjectName = project.value; //assign user input project name 
-          
-          projectList[newProjectName] = [newItem]; //add new project as key-value pair to projectList object
+if (edit === 'edit-'){
+  let editedItem = new Item(title.textContent, description.value, dueDate.value, priority.value, project.textContent);
+  localStorage.setItem(editedItem.identifier, JSON.stringify(editedItem));
+
+        // title.value = '';
+        description.value = '';
+        dueDate.value = '';
+        priority.value = '';
+        // project.value = '';
+}else {
+  if (title.value){//if title field is not blank then initialize a new item using item class
+    let newItem = new Item(title.value, description.value, dueDate.value, priority.value, project.value)
+    localStorage.setItem(`${project.value} ${title.value}`, JSON.stringify(newItem));
+  
+    let projectNameArray = Object.keys(projectList);
+    if (projectNameArray.includes(project.value)){ //if the item project name already exists
+      for (let i = 0; i < projectNameArray.length; i++) { 
+        if (project.value === projectNameArray[i]){ //find the name of the existing project in projectList
+          let matchingProjectName = project.value;
+          projectList[matchingProjectName].push(newItem);//add newItem to the project value array
+           }
         }
-    }
-
-    title.value = '';
-    description.value = '';
-    dueDate.value = '';
-    priority.value = '';
-    project.value = '';
-
+      } else { //when includes() returns false
+        let newProjectName = project.value; //assign user input project name 
+        
+        projectList[newProjectName] = [newItem]; //add new project as key-value pair to projectList object
+      }
+  }
+  //reset each field after items are added
+        title.value = '';
+        description.value = '';
+        dueDate.value = '';
+        priority.value = '';
+        project.value = '';
+      }
     popupNode.classList.remove('active');
   }
 
-  
+  export function displayItems(nameOfProject, itemList, projectList) {
+    let numberOfItemInStorage = localStorage.length;
 
-  
+    for (let i = 0; i < numberOfItemInStorage; i++){
+      //retrieve item key
+      let retrievedItemKey = localStorage.key(i);
+      //use the retrieved item key to get item value
+      let retrievedItemValue = localStorage.getItem(retrievedItemKey);
+      //parse the item value
+      let parsedItem = JSON.parse(retrievedItemValue);
 
-  //display item function
-  //this function takes an array of objects(todo items) as an argument to display the item elements in it
-  export function displayItems(projectItemArray, itemList, projectList) {
-    // let itemArray = Object.values(projectList);
-
-    for (let i = 0; i < projectItemArray.length; i ++){
+      //if project name === argument projectName, then list the item 
+        if (parsedItem.project === nameOfProject) {
       let item = document.createElement('li');
       let itemRemoveBtn = document.createElement('button');
       let itemTitle = document.createElement('div');
@@ -77,26 +90,26 @@ export class Item {
     // item.projectItemArray[i] = projectItemArray[i];
 
       let itemProject = document.createElement('div');
-      let projectName = projectItemArray[i].project;
+      let projectName = parsedItem.project;
       console.log(projectName);
       
       itemList.appendChild(item);
       console.log(`this is logging item number ${i}`)
       
-      itemTitle.textContent =`Title: ${projectItemArray[i].title}` ;
-      itemTitle.setAttribute('project-item', `${projectName} ${projectItemArray[i].title}`);
+      itemTitle.textContent =`Title: ${parsedItem.title}` ;
+      itemTitle.setAttribute('project-item', `${projectName} ${parsedItem.title}`);
       item.appendChild(itemTitle);
 
-      itemDescription.textContent = `Description: ${projectItemArray[i].description}`;
+      itemDescription.textContent = `Description: ${parsedItem.description}`;
       item.appendChild(itemDescription);
 
-      itemDueDate.textContent = `Due: ${projectItemArray[i].dueDate}`;
+      itemDueDate.textContent = `Due: ${parsedItem.dueDate}`;
       item.appendChild(itemDueDate);
 
-      itemPriority.textContent = `Priority: ${projectItemArray[i].priority}`;
+      itemPriority.textContent = `Priority: ${parsedItem.priority}`;
       item.appendChild(itemPriority);
 
-      itemProject.textContent = `Project: ${projectItemArray[i].project}`;
+      itemProject.textContent = `Project: ${parsedItem.project}`;
       item.appendChild(itemProject);
 
       //update style for each to do item attribute
@@ -111,13 +124,13 @@ export class Item {
       //need to reference the item object to implement the functionality to all the edit buttons
       editItemBtn.textContent = 'Edit';
       editItemBtn.classList.add('open-edit-popup');
-      console.log(projectItemArray[i].title);
-      editItemBtn.setAttribute('project-item', `${projectName} ${projectItemArray[i].title}`);
+      // console.log(projectItemArray[i].title);
+      editItemBtn.setAttribute('project-item', `${projectName} ${parsedItem.title}`);
       item.appendChild(editItemBtn);
 
       //remove item logic
       let btnIdentifier = editItemBtn.getAttribute('project-item');
-      let itemIndex = projectItemArray.indexOf(item);
+      // let itemIndex = projectItemArray.indexOf(item);
       itemRemoveBtn.addEventListener('click', (e) => {
         for (let project in projectList) {
           for (let i = 0; i < projectList[project].length; i++) {
@@ -133,10 +146,78 @@ export class Item {
         itemList.removeChild(item);
         // console.log(projectList);
       })
-      
+        }
     }
   }
-  
+
+  //display 1 item 
+  //input itemList for attaching the dom elements for individual item
+  export function displayItem (parsedItemObjectFromStorage, itemList, retrievedItemKey){
+      let item = document.createElement('li');
+      let itemRemoveBtn = document.createElement('button');
+      let itemTitle = document.createElement('div');
+      let itemDescription = document.createElement('div');
+      let itemDueDate = document.createElement('div');
+      let itemPriority = document.createElement('div');
+      let editItemBtn = document.createElement('button');
+
+      let itemProject = document.createElement('div');
+      let projectName = parsedItemObjectFromStorage.project; //need to take project name as a function input
+
+      itemList.appendChild(item);
+      
+      itemTitle.textContent =`Title: ${parsedItemObjectFromStorage.title}` ;
+      itemTitle.setAttribute('project-item', `${projectName} ${parsedItemObjectFromStorage.title}`);
+      item.appendChild(itemTitle);
+
+      itemDescription.textContent = `Description: ${parsedItemObjectFromStorage.description}`;
+      item.appendChild(itemDescription);
+
+      itemDueDate.textContent = `Due: ${parsedItemObjectFromStorage.dueDate}`;
+      item.appendChild(itemDueDate);
+
+      itemPriority.textContent = `Priority: ${parsedItemObjectFromStorage.priority}`;
+      item.appendChild(itemPriority);
+
+      itemProject.textContent = `Project: ${parsedItemObjectFromStorage.project}`;
+      item.appendChild(itemProject);
+
+      //update style for each to do item attribute
+
+      itemRemoveBtn.textContent = 'Remove';
+      itemRemoveBtn.classList.add('remove');
+      item.appendChild(itemRemoveBtn);
+
+      
+
+      //edit item logic
+      //need to reference the item object to implement the functionality to all the edit buttons
+      editItemBtn.textContent = 'Edit';
+      editItemBtn.classList.add('open-edit-popup');
+      // console.log(projectItemArray[i].title);
+      editItemBtn.setAttribute('project-item', `${projectName} ${parsedItemObjectFromStorage.title}`);
+      item.appendChild(editItemBtn);
+
+      //remove item logic
+      let btnIdentifier = editItemBtn.getAttribute('project-item');
+      // let itemIndex = projectItemArray.indexOf(item);
+      itemRemoveBtn.addEventListener('click', (e) => {
+        // for (let project in projectList) {
+        //   for (let i = 0; i < projectList[project].length; i++) {
+        //     if(projectList[project][i].identifier === btnIdentifier) {
+        //       projectList[project].splice(i, 1);
+        //       // console.log(projectList)
+        //     }
+        //   }
+        // }
+        // projectItemArray.splice(itemIndex, 1);
+        //remove item from local storage
+        localStorage.removeItem(retrievedItemKey);
+        //remove dom element
+        itemList.removeChild(item);
+        // console.log(projectList);
+      })
+  }
 
 
   // export function editItem(projectList, titleProjectRef){
