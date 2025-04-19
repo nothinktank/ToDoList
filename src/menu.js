f493bbddcd8b8786af7caa2c3826c91c3d90476a
shangwang <wangshangming10@gmail.com>
2025-04-19 13:49:15 -0400

    removed irrelevant files

--- src/menu.js
+++
@@ -1,9 +0,0 @@
-//function that contains the menu of the restaurant
-export function renderMenu(){
-  const menuDiv = document.createElement('div');
-
-  menuDiv.textContent = 'we serve power forwards and centers as entrees';
-
-  const contentDiv = document.querySelector('#content');
-  contentDiv.appendChild(menuDiv);
-}
